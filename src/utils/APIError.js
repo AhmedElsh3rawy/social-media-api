d29eacc1625fc5447ebfa75039ab8712a358e272
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-10 01:15:15 +0200

    chore: forgot 400

--- src/utils/APIError.js
+++ src/utils/APIError.js
@@ -3,13 +3,15 @@ class APIError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.statusText =
-      statusCode == 401
-        ? "Unauthorized"
-        : statusCode == 403
-          ? "Forbidden"
-          : statusCode == 404
-            ? "Not Found"
-            : "Internal Server Error";
+      statusCode == 400
+        ? "Bad request"
+        : statusCode == 401
+          ? "Unauthorized"
+          : statusCode == 403
+            ? "Forbidden"
+            : statusCode == 404
+              ? "Not Found"
+              : "Internal Server Error";
   }
 }
 
