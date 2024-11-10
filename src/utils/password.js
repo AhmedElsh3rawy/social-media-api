e271eff20276cd372eff173aac33acf408cbed8c
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-10 22:45:44 +0200

    fix: password salt

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 
 export const hashPassword = (password) => {
-  return bcrypt.hashSync(password, process.env.SALT_ROUNDS);
+  return bcrypt.hashSync(password, 10);
 };
 
 export const comparePasswords = (password, hashed) => {
