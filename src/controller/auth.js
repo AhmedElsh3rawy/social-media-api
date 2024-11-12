3a6f9bc3ed981f1c6692eaa227af683e7a371b71
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-12 22:32:15 +0200

    feat: login route

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,7 +1,10 @@
 import { db } from "../database/db.js";
+import { sql } from "drizzle-orm";
 import { users } from "../database/schema.js";
 import { asyncWrapper } from "../utils/asyncWrapper.js";
+import APIError from "../utils/APIError.js";
 import { hashPassword, comparePasswords } from "../utils/password.js";
+import { creatAccessToken, creatRefreshToken } from "../utils/jwt.js";
 
 export const register = asyncWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,8 +14,29 @@ export const register = asyncWrapper(async (req, res, next) => {
     username: username,
     email: email,
     password: hashed,
-    profileImage: req.file.path,
+    profileImage: req.file?.filename,
   });
 
   res.status(201).json({ statusCode: 201, status: "Created" });
 });
+
+export const login = asyncWrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await db.query.users.findFirst({
+    where: sql`${users.email} = ${email}`,
+  });
+  const matched = comparePasswords(password, user.password);
+  if (!user || !matched)
+    return next(new APIError("Invalid email or password", 400));
+
+  const accessToken = await creatAccessToken(user.id);
+  const refreshToken = await creatRefreshToken(user.id);
+
+  res.cookie("token", refreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  });
+  res.status(200).json({ token: accessToken });
+});
+
+export const logout = asyncWrapper(async (req, res, next) => { });
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { register } from "../controller/auth.js";
-import { registerValidator } from "../utils/validation/auth.validator.js";
+import { login, register } from "../controller/auth.js";
+import {
+  loginValidator,
+  registerValidator,
+} from "../utils/validation/auth.validator.js";
 import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
 router.post("/register", upload.single("image"), registerValidator, register);
+
+router.post("/login", login);
 
 export default router;
