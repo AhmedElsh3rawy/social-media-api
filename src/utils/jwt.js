e9faa68676fd1f1a06d8d192d40dca3eb2d68cf9
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-12 22:31:57 +0200

    feat: jwt helper

---
+++ src/utils/jwt.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export const creatAccessToken = async (userId) => {
+  return await jwt.sign({ id: userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15s",
+  });
+};
+
+export const creatRefreshToken = async (userId) => {
+  return await jwt.sign({ id: userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const verifyAccessToken = async (token) => {
+  return await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+};
+
+export const verifyRefreshToken = async (token) => {
+  return await jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+};
