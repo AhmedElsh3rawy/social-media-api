60ab3adc8c1b70f79d152aaa88fc07cdb93c2ddb
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-13 20:47:12 +0200

    feat(user routes): get user by id

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,6 +1,8 @@
 import { db } from "../database/db.js";
+import { eq } from "drizzle-orm";
 import { users } from "../database/schema.js";
 import { asyncWrapper } from "../utils/asyncWrapper.js";
+import APIError from "../utils/APIError.js";
 
 export const getUsers = asyncWrapper(async (req, res, next) => {
   const result = await db
@@ -12,4 +14,20 @@ export const getUsers = asyncWrapper(async (req, res, next) => {
     .from(users);
 
   res.status(200).json(result);
+});
+
+export const getUserById = asyncWrapper(async (req, res, next) => {
+  const userId = +req.params.id;
+  const user = await db
+    .select({
+      username: users.username,
+      bio: users.bio,
+      profileImage: users.profileImage,
+    })
+    .from(users)
+    .where(eq(users.id, userId));
+
+  if (!user)
+    return next(new APIError("User does not exist or invalid id"), 400);
+  res.status(200).json(user);
 });
--- src/router/user.js
+++ src/router/user.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controller/user.js";
+import { getUserById, getUsers } from "../controller/user.js";
 
 const router = Router();
 
 router.get("/users", getUsers);
+
+router.get("/users/:id", getUserById);
 
 export default router;
