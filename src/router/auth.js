1d47b79a935bd35db19479779aa4a4d93e9f1bd2
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-13 16:59:22 +0200

    feat: refresh and logout routes

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
-import { login, register } from "../controller/auth.js";
+import { login, logout, register } from "../controller/auth.js";
+import { refresh } from "../controller/refresh.js";
 import {
   loginValidator,
   registerValidator,
 } from "../utils/validation/auth.validator.js";
 import { upload } from "../middleware/multer.js";
+import { verifyJWT } from "../middleware/verifyJWT.js";
 
 const router = Router();
 
 router.post("/register", upload.single("image"), registerValidator, register);
 
-router.post("/login", login);
+router.post("/login", loginValidator, login);
+
+router.get("/logout", verifyJWT, logout);
+
+router.get("/refresh", refresh);
 
 export default router;
