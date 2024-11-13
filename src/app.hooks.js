887cf82a29efe1e3f3e2273e98911c652591cc61
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-13 17:05:10 +0200

    feat: use auth middleware globaly after auth router

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,9 +1,11 @@
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
+import { verifyJWT } from "./middleware/verifyJWT.js";
 import authRouter from "./router/auth.js";
 
 export default (app) => {
-  // routers
   app.use("/api/v1/auth", authRouter);
+  app.use(verifyJWT);
+  // all remaining routers here
   app.use("*", notFound);
   app.use(errorHandler);
 };
