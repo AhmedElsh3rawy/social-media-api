be7efd47ff99d4e7b7ab17d11ccc0be7187888a8
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-13 16:59:04 +0200

    feat: logout

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,6 +1,6 @@
 import { db } from "../database/db.js";
-import { sql } from "drizzle-orm";
-import { users } from "../database/schema.js";
+import { sql, eq } from "drizzle-orm";
+import { users, tokens } from "../database/schema.js";
 import { asyncWrapper } from "../utils/asyncWrapper.js";
 import APIError from "../utils/APIError.js";
 import { hashPassword, comparePasswords } from "../utils/password.js";
@@ -31,6 +31,7 @@ export const login = asyncWrapper(async (req, res, next) => {
 
   const accessToken = await creatAccessToken(user.id);
   const refreshToken = await creatRefreshToken(user.id);
+  await db.insert(tokens).values({ token: refreshToken });
 
   res.cookie("token", refreshToken, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -39,4 +40,11 @@ export const login = asyncWrapper(async (req, res, next) => {
   res.status(200).json({ token: accessToken });
 });
 
-export const logout = asyncWrapper(async (req, res, next) => { });
+export const logout = asyncWrapper(async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return next(new APIError("You are not logged in", 403));
+
+  await db.delete(tokens).where(eq(tokens.token, token));
+  res.clearCookie("token", { httpOnly: true });
+  res.status(200).json({ message: "Logged out successfully" });
+});
