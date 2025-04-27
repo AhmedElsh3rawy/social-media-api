9d349393f4805228543f17499fc470b799f95213
Ahmed <ahmedelsharawy117@gmail.com>
2025-04-27 03:49:10 +0300

    feat: use morgan as logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
 
 const PORT = (process.env.PORT as unknown as number) || 8080;
+
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, World!");
