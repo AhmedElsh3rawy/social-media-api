bce019d2e9ecc1c56aae4d22e4b245a81a30e50c
Ahmed <ahmedelsharawy117@gmail.com>
2025-04-30 00:32:00 +0300

    feat: connect to db client

---
+++ src/config/database/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+import * as schema from "./schema/schema";
+
+const client = new Client({
+	connectionString: process.env.DB_URL as string,
+});
+
+(async () => {
+	await client.connect();
+})();
+
+export const db = drizzle(client, { schema, logger: true });
