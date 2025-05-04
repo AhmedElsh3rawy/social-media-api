4e0312e3c272191342f2eaf9cf40b43fcef8bcb3
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-04 03:44:23 +0300

    feat: imagekit to upload to imagekit.io

--- src/config/image-kit.ts
+++ src/config/image-kit.ts
@@ -1,15 +1,19 @@
 import ImageKit from "imagekit";
+import type { NextFunction, Request } from "express";
+import fs from "node:fs";
+import type { ReadStream } from "node:fs";
 
 const imagekit = new ImageKit({
 	publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
-	privateKey: process.env.IMAGEKIT_PRIVAT_KEY as string,
+	privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
 	urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT as string,
 });
 
 export const uploadImage = async (req: Request) => {
+	const imgStream: ReadStream = fs.createReadStream(req.file.path);
 	const result = await imagekit.upload({
-		file: req.file?.buffer as Buffer<ArrayBufferLike>,
-		fileName: `${req.file?.filename}.png`,
+		file: imgStream,
+		fileName: req.file.filename,
 		tags: ["tag1", "tag2"],
 	});
 
