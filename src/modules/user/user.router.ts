56ea250af6d9ff90eb0be0776de6b7e0d031d3ea
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-04 23:26:17 +0300

    feat: add swagger doc to getAll and getById

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -5,8 +5,37 @@ import { validate } from "../../middleware/validation";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/v1/users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
 router.get("/", getAll);
 
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User found
+ *       404:
+ *         description: User not found
+ */
 router.get("/:id", validate(getByIdSchema, "params"), getById);
 
 export default router;
