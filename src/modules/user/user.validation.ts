52fd8e72339cfd3fd193e4de083eb93d8ef2e250
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-07 16:33:41 +0300

    feat: getByNameSchema validation

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -9,3 +9,10 @@ export const getByIdSchema = z.object({
 export const getByEmailSchema = z.object({
 	email: z.string().email({ message: "Enter a valid email." }),
 });
+
+export const getByNameSchema = z.object({
+	name: z
+		.string()
+		.min(2, { message: "Name should be at least 2 characters." })
+		.max(30, { message: "Name should be 15 characters at most." }),
+});
