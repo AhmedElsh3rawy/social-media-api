d094e41185dbf2e2049fde8d0b853ca9aa26aca6
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-07 16:07:59 +0300

    chore: rename file schema.ts to index.ts

--- src/config/database/db.ts
+++ src/config/database/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
-import * as schema from "./schema/schema";
+import * as schema from "./schema/index";
 
 const client = new Client({
 	connectionString: process.env.DATABASE_URL as string,
