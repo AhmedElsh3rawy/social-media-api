0c74415e811a99149473f6555b7e8d978ab2cf29
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-07 16:10:19 +0300

    feat: getByEmailSchema validation

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -5,3 +5,7 @@ export const getByIdSchema = z.object({
 		.number({ message: "Id must be a number." })
 		.gt(0, { message: "Id must be greater than 0." }),
 });
+
+export const getByEmailSchema = z.object({
+	email: z.string().email({ message: "Enter a valid email." }),
+});
