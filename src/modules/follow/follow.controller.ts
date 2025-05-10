acef2663800289e5a265a5cb62a4ea73cbc2035a
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-10 12:48:01 +0300

    feat: user unfollow another user

--- src/modules/follow/follow.controller.ts
+++ src/modules/follow/follow.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from "express";
 import { asyncWrapper } from "../../utils/asyncWrapper";
 import { db } from "../../config/database/db";
 import { follows } from "../../config/database/schema";
+import { and, eq } from "drizzle-orm";
 
 export const follow = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
@@ -11,5 +12,21 @@ export const follow = asyncWrapper(
 			.insert(follows)
 			.values({ followerId: followerId, followingId: followingId });
 		res.status(200).json({ message: "Followed successfully" });
+	},
+);
+
+export const unfollow = asyncWrapper(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const followerId = req.user.id;
+		const followingId = +req.params.id;
+		await db
+			.delete(follows)
+			.where(
+				and(
+					eq(follows.followerId, followerId),
+					eq(follows.followingId, followingId),
+				),
+			);
+		res.status(200).json({ message: "Unfollowed successfully" });
 	},
 );
--- src/modules/follow/follow.router.ts
+++ src/modules/follow/follow.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validate } from "../../middleware/validation";
 import { followSchema } from "./follow.validation";
-import { follow } from "./follow.controller";
+import { follow, unfollow } from "./follow.controller";
 
 const router = Router();
 
 router.post("/:id/follow", validate(followSchema, "params"), follow);
+
+router.delete("/:id/unfollow", validate(followSchema, "params"), unfollow);
 
 export default router;
