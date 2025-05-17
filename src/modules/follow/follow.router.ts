fa3caefe3623b6a517358a44b180a837adbb1ee1
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-17 11:41:53 +0300

    feat: validate of id param

--- src/modules/follow/follow.router.ts
+++ src/modules/follow/follow.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validate } from "../../middleware/validation";
-import { followSchema } from "./follow.validation";
+import { validateIdParam } from "../../utils/globalValidation";
 import {
 	follow,
 	getAllFollowers,
@@ -10,12 +10,36 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/v1/getAllFollowers:
+ *   get:
+ *     summary: Get all user followers
+ *     tags: [Follow]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
 router.get("/getAllFollowers", getAllFollowers);
 
+/**
+ * @swagger
+ * /api/v1/getAllFollowings:
+ *   get:
+ *     summary: Get all user followings
+ *     tags: [Follow]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
 router.get("/getAllFollowings", getAllFollowing);
 
-router.post("/:id/follow", validate(followSchema, "params"), follow);
+router.post("/:id/follow", validate(validateIdParam, "params"), follow);
 
-router.delete("/:id/unfollow", validate(followSchema, "params"), unfollow);
+router.delete("/:id/unfollow", validate(validateIdParam, "params"), unfollow);
 
 export default router;
