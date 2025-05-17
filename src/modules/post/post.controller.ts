4f7c7e05c4a5ea3089fcf71b9556cd387181eea2
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-17 11:43:26 +0300

    feat: delete post

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -4,6 +4,9 @@ import { asyncWrapper } from "../../utils/asyncWrapper";
 import type { CreatePostBody } from "./post.types";
 import { posts } from "../../config/database/schema";
 import { uploadImage } from "../../config/image-kit";
+import { eq } from "drizzle-orm";
+import { ownsPost } from "../../utils/ownsX";
+import AppError from "../../utils/appError";
 
 export const createPost = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
@@ -14,5 +17,17 @@ export const createPost = asyncWrapper(
 		}
 		const result = await db.insert(posts).values(data).returning();
 		res.status(200).json({ data: result[0] });
+	},
+);
+
+export const deletePost = asyncWrapper(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const id = +req.params.id;
+		const check = await ownsPost(req.user.id, id);
+		if (!check) {
+			return next(new AppError("You do not own this post.", 403));
+		}
+		await db.delete(posts).where(eq(posts.id, id));
+		res.status(200).json({ message: "Post was deleted successfully." });
 	},
 );
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createPost } from "./post.controller";
+import { createPost, deletePost } from "./post.controller";
 import { validate } from "../../middleware/validation";
-import { createPostSchema } from "./post.validation";
+import { createPostSchema, deletePostSchema } from "./post.validation";
+import { validateIdParam } from "../../utils/globalValidation";
 
 const router = Router();
 
 router.post("/", validate(createPostSchema, "body"), createPost);
+
+router.delete("/:id", validate(validateIdParam, "params"), deletePost);
 
 export default router;
