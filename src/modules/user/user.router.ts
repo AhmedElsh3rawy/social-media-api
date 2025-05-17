5c75ad79b8cc7ac48c090da6def30e816a4eeea0
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-17 14:31:02 +0300

    chore: remove swagger comments

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -19,100 +19,12 @@ import { verifyJWT } from "../../middleware/verifyJWT";
 
 const router = Router();
 
-/**
- * @swagger
- * /api/v1/users:
- *   get:
- *     summary: Get all users
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: A list of users
- */
 router.get("/", getAll);
 
-/**
- * @swagger
- * /api/v1/users/name:
- *   get:
- *     summary: Get users by name
- *     tags: [Users]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: query
- *         name: name
- *         required: true
- *         description: The name of the user
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Users found
- *       400:
- *         description: No user by that name
- */
 router.get("/name", verifyJWT, validate(getByNameSchema, "query"), getByName);
 
-/**
- * @swagger
- * api/v1/users/me:
- *   patch:
- *     summary: Update the current user
- *     tags: [Users]
- *     security:
- *       - BearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *     responses:
- *       200:
- *         description: Username updated successfully
- *       401:
- *         description: Unauthorized
- */
 router.patch("/me", verifyJWT, validate(updateUserSchema, "body"), updateUser);
 
-/**
- * @swagger
- * /api/v1/users/profile-picture:
- *   patch:
- *     summary: Change the user's profile picture
- *     tags: [Users]
- *     security:
- *       - BearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               image:
- *                 type: string
- *                 format: binary
- *     responses:
- *       200:
- *         description: Profile picture updated successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: string
- *                     imageUrl:
- *                       type: string
- */
 router.patch(
 	"/profile-picture",
 	verifyJWT,
@@ -120,50 +32,8 @@ router.patch(
 	changeProfilePic,
 );
 
-/**
- * @swagger
- * /api/v1/users/{id}:
- *   get:
- *     summary: Get user by ID
- *     tags: [Users]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: The ID of the user
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: User found
- *       404:
- *         description: User not found
- */
 router.get("/:id", verifyJWT, validate(validateIdParam, "params"), getById);
 
-/**
- * @swagger
- * /api/v1/users/{email}/getByEmail:
- *   get:
- *     summary: Get user by email
- *     tags: [Users]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: email
- *         required: true
- *         description: The email of the user
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: User found
- *       404:
- *         description: User not found
- */
 router.get(
 	"/:email/getByEmail",
 	verifyJWT,
