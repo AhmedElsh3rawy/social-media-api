c792c6aa168c497d811a96d47a05f6b03213ca7c
Ahmed <ahmedelsharawy117@gmail.com>
2025-05-23 19:33:13 +0300

    feat(db): add commentLikes table

--- src/config/database/schema/like.ts
+++ src/config/database/schema/like.ts
@@ -1,6 +1,7 @@
 import { pgTable, integer, primaryKey } from "drizzle-orm/pg-core";
 import { users } from "./user";
 import { posts } from "./post";
+import { comments } from "./comment";
 
 export const PostLikes = pgTable(
 	"post_likes",
@@ -15,5 +16,24 @@ export const PostLikes = pgTable(
 	},
 	(table) => ({
 		pk: primaryKey({ columns: [table.userId, table.postId] }),
+	}),
+);
+
+export const CommentLikes = pgTable(
+	"comment_likes",
+	{
+		userId: integer("user_id")
+			.notNull()
+			.references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
+
+		commentId: integer("post_id")
+			.notNull()
+			.references(() => comments.id, {
+				onDelete: "cascade",
+				onUpdate: "cascade",
+			}),
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.userId, table.commentId] }),
 	}),
 );
