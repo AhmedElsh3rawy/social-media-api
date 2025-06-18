6153e1d6d9c04740d13bb5a2f724281bfc47c3b3
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-18 22:42:09 +0300

    feat: togglePostLikes & getAllPostLikes

--- src/modules/like/like.controller.ts
+++ src/modules/like/like.controller.ts
@@ -1,35 +1,35 @@
 import { db } from "../../config/database/db";
-import { likes } from "../../config/database/schema";
+import { postLikes, commentLikes } from "../../config/database/schema";
 import { and, eq } from "drizzle-orm";
 import type { Request, Response, NextFunction } from "express";
 import { asyncWrapper } from "../../utils/asyncWrapper";
 
-export const toggleLike = asyncWrapper(
+export const togglePostLike = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
 		const id = req.user.id;
 		const postId = +req.params.id;
 		const result = await db
 			.select()
-			.from(likes)
-			.where(and(eq(likes.userId, id), eq(likes.postId, id)));
+			.from(postLikes)
+			.where(and(eq(postLikes.userId, id), eq(postLikes.postId, id)));
 		if (result.length === 0) {
-			await db.insert(likes).values({ userId: id, postId: postId });
+			await db.insert(postLikes).values({ userId: id, postId: postId });
 			return res.status(200).json({ message: "Liked" });
 		}
 		await db
-			.delete(likes)
-			.where(and(eq(likes.userId, id), eq(likes.postId, postId)));
+			.delete(postLikes)
+			.where(and(eq(postLikes.userId, id), eq(postLikes.postId, postId)));
 		res.status(200).json({ message: "Disliked" });
 	},
 );
 
-export const getAllLikes = asyncWrapper(
+export const getAllPostLikes = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
 		const postId = +req.params.id;
 		const result = await db
 			.select()
-			.from(likes)
-			.where(eq(likes.postId, postId));
+			.from(postLikes)
+			.where(eq(postLikes.postId, postId));
 		res.status(200).json({ data: result });
 	},
 );
