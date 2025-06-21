bc893b8bcb5455dc0cf3cd1a069bab8a8a2dd0d0
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-21 15:54:52 +0300

    feat: add owns comment function

--- src/utils/ownsX.ts
+++ src/utils/ownsX.ts
@@ -11,3 +11,11 @@ export const ownsShare = async (userId: number, shareId: number) => {
 	const result = await db.select().from(shares).where(eq(shares.id, shareId));
 	return result.length === 0 ? false : result[0].authorId === userId;
 };
+
+export const ownsComment = async (userId: number, commentId: number) => {
+	const result = await db
+		.select()
+		.from(comments)
+		.where(eq(comments.id, commentId));
+	return result.length === 0 ? false : result[0].authorId === userId;
+};
