b8a108a15ee85d76c165fb6be7ccf73d59b8fa3d
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-21 15:53:21 +0300

    feat: add own share function

--- src/utils/ownsX.ts
+++ src/utils/ownsX.ts
@@ -1,8 +1,13 @@
 import { eq } from "drizzle-orm";
 import { db } from "../config/database/db";
-import { posts } from "../config/database/schema";
+import { posts, shares, comments } from "../config/database/schema";
 
 export const ownsPost = async (userId: number, postId: number) => {
 	const result = await db.select().from(posts).where(eq(posts.id, postId));
+	return result.length === 0 ? false : result[0].authorId === userId;
+};
+
+export const ownsShare = async (userId: number, shareId: number) => {
+	const result = await db.select().from(shares).where(eq(shares.id, shareId));
 	return result.length === 0 ? false : result[0].authorId === userId;
 };
