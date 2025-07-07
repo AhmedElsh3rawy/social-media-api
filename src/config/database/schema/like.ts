72878464fa8aebfe96ee940b2ffb15cabf3c5690
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-08 01:15:10 +0300

    feat: combine posts and comments likes to one table

--- src/config/database/schema/like.ts
+++ src/config/database/schema/like.ts
@@ -1,39 +1,16 @@
-import { pgTable, integer, primaryKey } from "drizzle-orm/pg-core";
+import { pgTable, integer, text, primaryKey } from "drizzle-orm/pg-core";
 import { users } from "./user";
-import { posts } from "./post";
-import { comments } from "./comment";
 
-export const postLikes = pgTable(
-	"post_likes",
+export const likes = pgTable(
+	"likes",
 	{
 		userId: integer("user_id")
 			.notNull()
 			.references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
-
-		postId: integer("post_id")
-			.notNull()
-			.references(() => posts.id, { onDelete: "cascade", onUpdate: "cascade" }),
+		targetId: integer("target_id").notNull(),
+		type: text("type").notNull(),
 	},
 	(table) => ({
-		pk: primaryKey({ columns: [table.userId, table.postId] }),
-	}),
-);
-
-export const commentLikes = pgTable(
-	"comment_likes",
-	{
-		userId: integer("user_id")
-			.notNull()
-			.references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
-
-		commentId: integer("comment_id")
-			.notNull()
-			.references(() => comments.id, {
-				onDelete: "cascade",
-				onUpdate: "cascade",
-			}),
-	},
-	(table) => ({
-		pk: primaryKey({ columns: [table.userId, table.commentId] }),
+		pk: primaryKey({ columns: [table.userId, table.targetId, table.type] }),
 	}),
 );
