36649148e52a08f9137bac016bb5cf4ba0d97f6f
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-09 02:57:01 +0300

    feat: combine follow and unfollow in one function

--- src/modules/follow/follow.controller.ts
+++ src/modules/follow/follow.controller.ts
@@ -3,22 +3,32 @@ import { asyncWrapper } from "../../utils/asyncWrapper";
 import { db } from "../../config/database/db";
 import { follows, users } from "../../config/database/schema";
 import { and, eq } from "drizzle-orm";
+import AppError from "../../utils/appError";
 
-export const follow = asyncWrapper(
+export const toggleFollow = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
 		const followerId = req.user.id;
 		const followingId = +req.params.id;
-		await db
-			.insert(follows)
-			.values({ followerId: followerId, followingId: followingId });
-		res.status(200).json({ message: "Followed successfully" });
-	},
-);
 
-export const unfollow = asyncWrapper(
-	async (req: Request, res: Response, next: NextFunction) => {
-		const followerId = req.user.id;
-		const followingId = +req.params.id;
+		if (followerId === followingId) {
+			return next(new AppError("You can not follow yourself", 400));
+		}
+
+		const existing = await db
+			.select()
+			.from(follows)
+			.where(
+				and(
+					eq(follows.followerId, followerId),
+					eq(follows.followingId, followingId),
+				),
+			);
+
+		if (existing.length === 0) {
+			await db.insert(follows).values({ followerId, followingId });
+			return res.status(200).json({ message: "Followed successfully" });
+		}
+
 		await db
 			.delete(follows)
 			.where(
@@ -27,50 +37,7 @@ export const unfollow = asyncWrapper(
 					eq(follows.followingId, followingId),
 				),
 			);
+
 		res.status(200).json({ message: "Unfollowed successfully" });
-	},
-);
-
-export const getMyFollowers = asyncWrapper(
-	async (req: Request, res: Response, next: NextFunction) => {
-		const id = req.user.id;
-		const result = await db
-			.select({ follower: users })
-			.from(users)
-			.innerJoin(follows, eq(follows.followerId, users.id));
-		res.status(200).json({ data: result });
-	},
-);
-
-export const getMyFollowing = asyncWrapper(
-	async (req: Request, res: Response, next: NextFunction) => {
-		const id = req.user.id;
-		const result = await db
-			.select({ following: users })
-			.from(users)
-			.innerJoin(follows, eq(follows.followingId, users.id));
-		res.status(200).json({ data: result });
-	},
-);
-
-export const getAllFollowers = asyncWrapper(
-	async (req: Request, res: Response, next: NextFunction) => {
-		const id = +req.params.id;
-		const result = await db
-			.select({ follower: users })
-			.from(users)
-			.innerJoin(follows, eq(follows.followerId, users.id));
-		res.status(200).json({ data: result });
-	},
-);
-
-export const getAllFollowing = asyncWrapper(
-	async (req: Request, res: Response, next: NextFunction) => {
-		const id = +req.params.id;
-		const result = await db
-			.select({ following: users })
-			.from(users)
-			.innerJoin(follows, eq(follows.followingId, users.id));
-		res.status(200).json({ data: result });
 	},
 );
--- src/modules/follow/follow.router.ts
+++ src/modules/follow/follow.router.ts
@@ -1,35 +1,10 @@
 import { Router } from "express";
 import { validate } from "../../middleware/validation";
 import { validateIdParam } from "../../utils/globalValidation";
-import {
-	follow,
-	getAllFollowers,
-	getAllFollowing,
-	getMyFollowers,
-	getMyFollowing,
-	unfollow,
-} from "./follow.controller";
+import { toggleFollow } from "./follow.controller";
 
 const router = Router();
 
-router.get("/following/me", getMyFollowing);
-
-router.get("/followers/me", getMyFollowers);
-
-router.get(
-	"/followers/:id",
-	validate(validateIdParam, "params"),
-	getAllFollowers,
-);
-
-router.get(
-	"/following/:id",
-	validate(validateIdParam, "params"),
-	getAllFollowing,
-);
-
-router.post("/follow/:id", validate(validateIdParam, "params"), follow);
-
-router.delete("/unfollow/:id", validate(validateIdParam, "params"), unfollow);
+router.post("/:id", validate(validateIdParam, "params"), toggleFollow);
 
 export default router;
